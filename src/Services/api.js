a9fa171de78f5c7a73d9af2f188dbe460af86a84
Pedro Lucas <pedrolucas122737@gmail.com>
2024-01-21 18:33:13 -0300

    Configurações deploy

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7065/api/",
+  baseURL: "https://alunosapi.azurewebsites.net/api/",
 });
 
 export default api;
